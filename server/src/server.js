e979383c60632f13b7dfd154d7d4368e41cef0c0
IAmJerryJ <jerryjia1999429@gmail.com>
2023-04-24 10:40:22 +1000

    Add mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,23 @@ const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:12345caomao1@nasacluster.g2u58fq.mongodb.net/?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
     await loadPlanetsData();
     
